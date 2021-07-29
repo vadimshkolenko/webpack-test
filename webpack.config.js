2de5d48ac03c6f44e76fb988399878f490d07597
Vadim Shkolenko <vadimand1998@gmail.com>
2021-07-29 09:02:50 +0300

    доработки

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,28 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    }
+  }
+
+  if (!isDev) {
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserWebpackPlugin(),
+    ]
+  }
+
+  return config
+}
 
 module.exports = {
   // указывает где лежат все исходники (можем из путей убрать src/)
@@ -26,16 +48,16 @@ module.exports = {
       'models': path.resolve(__dirname, 'src/models'),
     },
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    }
-  },
+  optimization: optimization(),
   plugins: [
       // создает html в dist и подключает чанки
       new HTMLWebpackPlugin({
         // указываем основной html файл чтобы подтянуть содержимое из него
-        template: "./index.html"
+        template: "./index.html",
+        // исп для оптимизации html
+        minify: {
+          collapseWhitespace: !isDev,
+        }
       }),
       // используется для удаления ненужных чанков
       new CleanWebpackPlugin(),
@@ -44,6 +66,9 @@ module.exports = {
           from: path.resolve(__dirname, 'src/assets/favicon.ico'),
           to: path.resolve(__dirname, 'dist'),
         }]
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
       })
   ],
   // для возможности работы не только с js, json
@@ -55,7 +80,18 @@ module.exports = {
           // Используем лодеры для данных форматов. Обработка идет справа налево
           // css-loader - позволяет исп импорты
           // style-loader - добавляет стили в секцию head в html
-          use: ['style-loader', 'css-loader']
+          use: [
+            //если хотим добавить опции записываем как объект
+            {
+              loader: MiniCssExtractPlugin.loader,
+              // options: {
+              //   // изменения вносятся без перзагрузки проекта
+              //   hrm: isDev,
+              //   reloadAll: true,
+              // }
+            },
+            'css-loader'
+          ]
         },
         {
           test: /\.(png|jpeg|svg|gif)$/,
@@ -70,6 +106,7 @@ module.exports = {
  },
   // предварительно ставим webpack-dev-server, исп для мгновенного отображения изменений файлов
   devServer: {
-    port: 4200
+    port: 4200,
+    hot: isDev,
   }
 }
